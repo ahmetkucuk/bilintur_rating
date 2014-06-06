0c753181a4973f76c5290535dff0b416b41d2337
Ahmet Kucuk <ahmetkucuk92@gmail.com>
2014-06-06 04:57:15 +0200

    edit settings.py

--- bilintur_rating/settings.py
+++ bilintur_rating/settings.py
@@ -47,17 +47,6 @@ ROOT_URLCONF = 'bilintur_rating.urls'
 
 WSGI_APPLICATION = 'bilintur_rating.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
 
