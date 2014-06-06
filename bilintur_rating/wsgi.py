b3d512f605485ba8f73b46bfecbdd08bd5fb361a
Ahmet Kucuk <ahmetkucuk92@gmail.com>
2014-06-06 05:07:15 +0200

    template used

--- bilintur_rating/wsgi.py
+++ bilintur_rating/wsgi.py
@@ -10,7 +10,8 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bilintur_rating.settings")
 
+# This application object is used by any WSGI server configured to use this
+# file. This includes Django's development server, if the WSGI_APPLICATION
+# setting points here.
 from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
-
-application = Cling(get_wsgi_application())
+application = get_wsgi_application()
